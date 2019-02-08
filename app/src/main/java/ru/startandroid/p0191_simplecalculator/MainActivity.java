9256b6070fb48e615839571c2708e3ad921e13e5
Admin <maltyncev@gmail.com>
2019-02-08 22:47:02 +0500

    08.02 22.46

--- app/src/main/java/ru/startandroid/p0191_simplecalculator/MainActivity.java
+++ app/src/main/java/ru/startandroid/p0191_simplecalculator/MainActivity.java
@@ -3,12 +3,17 @@ package ru.startandroid.p0191_simplecalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final int MENU_RESET_ID = 1;
+    final int MENU_QUIT_ID = 2;
 
     EditText etNum1;
     EditText etNum2;
@@ -89,5 +94,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // формируем строку вывода
         tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
+    }
+
+    // создание меню
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, MENU_RESET_ID, 0, "Reset");
+        menu.add(0, MENU_QUIT_ID, 0, "Quit");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // обработка нажатий на пункты меню
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET_ID:
+                // очищаем поля
+                etNum1.setText("");
+                etNum2.setText("");
+                tvResult.setText("");
+                break;
+            case MENU_QUIT_ID:
+                // выход из приложения
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
